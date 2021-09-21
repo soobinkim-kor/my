58109bc96c9639d37ed53982ea608b3b1d387a05
soobinkim-kor <72695318+soobinkim-kor@users.noreply.github.com>
2021-09-21 07:07:36 +0000

    숫자 카드 게임

--- p92 큰 수의 법칙/main.py
+++ p92 큰 수의 법칙/main.py
@@ -2,14 +2,15 @@ def greedy(n, m, k, numberList):
     numberList.sort()
     first = numberList[n - 1]
     second = numberList[n - 2]
-    count = int(m / (k+1))
-    r = m % (k+1)
+    count = int(m / (k + 1))
+    r = m % (k + 1)
     if (first == second):
         return first * m
     result = 0
     result = result + count * (first * k + second)
     result = result + r * first
     return result
-    
-numberList=[2,4,5,4,6]
-greedy(5,8,3,numberList)
+
+
+numberList = [2, 4, 5, 4, 6]
+greedy(5, 8, 3, numberList)
