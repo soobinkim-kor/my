7523f7a2f4bd14b841d02e9622be867fbdcb97e6
soobinkim-kor <72695318+soobinkim-kor@users.noreply.github.com>
2021-09-24 14:00:44 +0000

    2021/09/24

--- main.py
+++ main.py
@@ -1,35 +1,19 @@
-n=5
-shop=[8,3,7,9,2]
 
-m=3
-request=[5,7,9]
+a=[1,2,3,4]
+b=[4,5,6,2,1]
+c=[1,2,3,4,5,6,7,8,9,10]
 
-shop.sort()
+# 길이가 홀수일때, 짝수일 때 로 나눠서 생각
+# 길이가 짝수이면 길이/2 까지 바꾸면 됨.
+# 길이가 홀수이면 가운데 숫자를 제외하고 다 바꾸면 됨
+# (길이/2) 를 소수점 버림하면 위 경우를 모두 충족
 
-print(shop)
-print(request)
+def FlipList(a):
+  length=len(a)
+  for i in range(len(a)//2):
+    a[i], a[length-i-1] = a[length-i-1], a[i]
+  return a
 
-def binarySearch(array,target,start,end):
-  if(start>end):
-    return None
-
-  mid=(start+end)//2
-  
-  if(array[mid]==target):
-    return mid
-  
-  elif(array[mid]>target):
-    return binarySearch(array,target,start,mid-1)
-  
-  else:
-    return binarySearch(array,target,mid+1,end)
-
-answer=""
-
-for i in range(m):
-  result=binarySearch(shop,request[i],0,n-1)
-  if(result==None):
-    answer+="no "
-  else:
-    answer+="yes "
-print(answer)
+print(FlipList(a))
+print(FlipList(b))
+print(FlipList(c))
