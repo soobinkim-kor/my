90b6dee396498661984ebd8267d826616a2633c2
soobinkim-kor <72695318+soobinkim-kor@users.noreply.github.com>
2021-09-25 05:53:39 +0000

    2021/09/25

--- ch07 이진 탐색/p197 부품 찾기/p197.py
+++ ch07 이진 탐색/p197 부품 찾기/p197.py
@@ -7,12 +7,20 @@ request=[5,7,9]
 shop.sort()
 request.sort()
 result=[]
-for i in range(len(m)):
-  for j in range(len(n)):
-    if(request[i]==shop[j]):
-      result+="yes"
-  
-    elif(j==len(n)-1):
-      result+="no"
 
+def binarySearch(array,target,start,end):
+  if start>end:
+    return None
+  mid=(start+end)//2
+  if(array[mid]==target):
+    return mid
+  elif array[mid]>target:
+    return binarySearch(array,target,start,mid-1)
+  else:
+    return binarySearch(array,target,mid+1,end)
 
+for requests in request:
+  if(binarySearch(shop,requests,0,len(shop)-1)!=None):
+    print("yes",end=' ')
+  else:
+    print("no",end=' ')
--- main.py
+++ main.py
@@ -1,19 +1,26 @@
+n=5
+shop=[8,3,7,9,2]
 
-a=[1,2,3,4]
-b=[4,5,6,2,1]
-c=[1,2,3,4,5,6,7,8,9,10]
+m=3
+request=[5,7,9]
 
-# 길이가 홀수일때, 짝수일 때 로 나눠서 생각
-# 길이가 짝수이면 길이/2 까지 바꾸면 됨.
-# 길이가 홀수이면 가운데 숫자를 제외하고 다 바꾸면 됨
-# (길이/2) 를 소수점 버림하면 위 경우를 모두 충족
+shop.sort()
+request.sort()
+result=[]
 
-def FlipList(a):
-  length=len(a)
-  for i in range(len(a)//2):
-    a[i], a[length-i-1] = a[length-i-1], a[i]
-  return a
+def binarySearch(array,target,start,end):
+  if start>end:
+    return None
+  mid=(start+end)//2
+  if(array[mid]==target):
+    return mid
+  elif array[mid]>target:
+    return binarySearch(array,target,start,mid-1)
+  else:
+    return binarySearch(array,target,mid+1,end)
 
-print(FlipList(a))
-print(FlipList(b))
-print(FlipList(c))
+for requests in request:
+  if(binarySearch(shop,requests,0,len(shop)-1)!=None):
+    print("yes",end=' ')
+  else:
+    print("no",end=' ')
