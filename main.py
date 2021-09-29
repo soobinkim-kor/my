147eb26a8935c563983643ee3a86eaed84af3ab2
soobinkim-kor <72695318+soobinkim-kor@users.noreply.github.com>
2021-09-29 14:24:42 +0000

    2021/09/29

--- main.py
+++ main.py
@@ -5,12 +5,14 @@ def lucas(n):
     return 1
   return lucas(n-1)+lucas(n-2)
 
-def f(n):
-  if n==1 or n==2:
-    return 1
-  return f(n-1)+f(n-2)
+
 
 def lucas2(n):
+  def f(n):
+    if n==1 or n==2:
+      return 1
+    return f(n-1)+f(n-2)
+
   return f(2*n)-lucas(n)-f(n)
 
 print(lucas(1))
@@ -19,17 +21,11 @@ print(lucas(3))
 print(lucas(4))
 print(lucas(5))
 
-print(f(1))  
-print(f(2))
-print(f(3))
-print(f(4))
-print(f(5))
+
 
 print(lucas2(1))
-print(lucas2(2))
+print(lucas2(2))                                                                                                
 print(lucas2(3))
 print(lucas2(4))
 print(lucas2(5))
 
-
-2 1 3 4 
