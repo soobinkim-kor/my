f020dfc17d4aeb438d82f45146ab9ebf1f74a3b8
soobinkim-kor <72695318+soobinkim-kor@users.noreply.github.com>
2021-10-05 12:19:09 +0000

    2021/10/05

--- main.py
+++ main.py
@@ -1,33 +1,36 @@
-from collections import deque
-def solution(bridge_length, weight, truck_weights):
-    #bridge_lenth = 다리의 길이 / 올라갈 수 있는 트럭의 갯수  1 이상 10,000 이하
-    #weight = 다리가 버틸 수 있는 무게                      1 이상 10,000 이하
-    #truck_weights = 트럭 각각의 무게 / 순서대로 건너야 함    길이 1 이상 10,000 이하, 1 이상 weight 이하
-    #answer = 걸리는 최소 시간 (초)                        
-    
-    answer = 0
-    queue=deque()
-    for truck in truck_weights:        
-        queue.append(truck)
-    count=0
-    q = queue.popleft()
-    print(queue)
-    
+def bubble2(A):
+  for i in range(len(A)-1):
+    for j in range(len(A)-i-1):
+      if A[j]<A[j+1]:
+        A[j],A[j+1]=A[j+1],A[j]
+        print(A)
 
-    """
-    while(queue):
-        current=queue.popleft()
-        while(count<=weight):
-            if queue[0]+current > weight:
-                count+=1
-                continue
-            else:
-                count+=1
-                
-        current=queue.popleft()
-        
-            
-        answer+=1
-        break
-        """
-    return answer
+a=[1,2,5,7,8,3,4]
+
+b=[1,2,5,7,8,3,4]
+
+def bubble23(A):
+  for i in range(0,len(A)):
+    for j in range(0,len(A)-i-1):
+      if A[j]<A[j+1]:
+        A[j],A[j+1]=A[j+1],A[j]
+        print(A)
+
+
+bubble2(a)
+print()
+bubble23(b)
+# def bubble2(A):
+#   print(A)
+#   for i in range(0,len(A)):
+#     for j in range(0,len(A)-i-1):
+#       if A[j]<A[j+1]:
+#         A[j],A[j+1]=A[j+1],A[j]
+#         print(A)
+#   return A
+
+
+
+
+  
+# bubble2([1,2,5,7])
