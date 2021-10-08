4b43103d4bf4185cbc5c1649601ea41ef12287bc
soobinkim-kor <72695318+soobinkim-kor@users.noreply.github.com>
2021-10-08 08:17:10 +0000

    2021/10/08

--- ch13 DFS,BFS문제/p339.py
+++ ch13 DFS,BFS문제/p339.py
@@ -1,31 +1,31 @@
 import sys
 from collections import deque
 
-input=sys.stdin.readline
+input = sys.stdin.readline
 
-n,m,k,x=map(int,input().split())
+n, m, k, x = map(int, input().split())
 
-graph=[[] for _ in range(n+1)]
+graph = [[] for _ in range(n + 1)]
 for _ in range(m):
-  a,b=map(int,input().split())
-  graph[a].append(b)
+    a, b = map(int, input().split())
+    graph[a].append(b)
 print(graph)
-distance=[-1]*(n+1)
-distance[x]=0
+distance = [-1] * (n + 1)
+distance[x] = 0
 
-queue=deque([x])
-while(queue):
-  print(queue)
-  current=queue.popleft()
-  for node in graph[current]:
-    if distance[node]==-1:
-      distance[node]=distance[current]+1
-      queue.append(node)
-has=False
+queue = deque([x])
+while (queue):
+    print(queue)
+    current = queue.popleft()
+    for node in graph[current]:
+        if distance[node] == -1:
+            distance[node] = distance[current] + 1
+            queue.append(node)
+has = False
 for i in range(len(distance)):
-  if distance[i]==k:
-    print(i)
-    has=True
+    if distance[i] == k:
+        print(i)
+        has = True
 
-if has==False:
-  print(-1)
+if has == False:
+    print(-1)
