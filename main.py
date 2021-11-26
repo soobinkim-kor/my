aabd420cf8507d36bb5f3bc0da5f18c0f831c2c9
soobinkim-kor <72695318+soobinkim-kor@users.noreply.github.com>
2021-11-26 10:02:32 +0000

    21/11/26

--- main.py
+++ main.py
@@ -1,35 +1,31 @@
-def perm(A,r):
-    res=[]
-    if r==1:
-        for item in A:
-            res.append([item])
-        return res
-    for i in range(0,len(A)):
-        B=A[:i]+A[i+1:]
-        for permutation in perm(B,r-1):
-            res.append([A[i]]+permutation)
-    return res
-def bubblesort(S):
-    for i in range(len(S)):
-        for j in range(len(S)-i-1):
-            if S[j]>S[j+1]:
-                S[j],S[j+1]=S[j+1],S[j]
-    return S
-def combinations(n,r):
-  answer=[]
-  numbers = list(range(1, n+1, 1))
-  permlist=perm(numbers,r)
-  for perms in permlist:
-    perms=bubblesort(perms)
-  
-  for perms in permlist:
-    if perms not in answer:
-      answer.append(perms)
-  
-  return answer
-  print(answer)
+#배주항등식
+def extended_gcd(a,b):
+  if a==0:
+    return [0,1,b]
+  else:
+    c = extended_gcd(b%a,a)
+    x,y,d=c[0],c[1],c[2]
+    return [y-(b//a)*x,x,d]
+
+def bezout3(a,b,c):
+  i=extended_gcd(a,b)
+  j=extended_gcd(i[2],c)
+  return [i[0]*j[0],i[1]*j[0],j[1]]
 
 
+def gcd(a,b):
+  if a==0: return b
+  elif b==0: return a
+  elif a==b: return a
+  elif a>b: return gcd(a-b,b)
+  return gcd(a,b-a)
 
+def gcd2(a,b):
+  if a==0:return b
+  return gcd(b%a,a)
 
-combinations(4,3)
+def lcm(a,b):
+  return a*b/gcd(a,b)
+
+print(bezout3(2,3,5))
+print(bezout3(4,6,9))
