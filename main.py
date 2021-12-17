613fce560624b06adfb15607caf2551f71581db8
soobinkim-kor <72695318+soobinkim-kor@users.noreply.github.com>
2021-12-17 08:00:47 +0000

    21/12/17

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
-#배주항등식
-def extended_gcd(a,b):
-  if a==0:
-    return [0,1,b]
-  else:
-    c = extended_gcd(b%a,a)
-    x,y,d=c[0],c[1],c[2]
-    return [y-(b//a)*x,x,d]
+#우애수 찾기
+# DP로 풀기
 
-def bezout3(a,b,c):
-  i=extended_gcd(a,b)
-  j=extended_gcd(i[2],c)
-  return [i[0]*j[0],i[1]*j[0],j[1]]
+def divisorSum(n):  # 자기 자신을 제외한 약수의 합을 구하는 함수
+  sum=0
+  for i in range(1,n//2+1):
+    if n%i==0:
+      sum+=i
+  return sum
 
+def chain(n,a): #적어도 1개 숫자가 a 이상인 n 길이의 친화수
+  numberDivSum=dict()
+  #divisorSumMap=[]  #인덱스 0번째 = 0.  
+  for i in range(a+1):
+    numberDivSum[i]=[divisorSum(i)]
+  numberDivSum=sorted(numberDivSum.items(),key=lambda item: item[1])
+  return numberDivSum
 
-def gcd(a,b):
-  if a==0: return b
-  elif b==0: return a
-  elif a==b: return a
-  elif a>b: return gcd(a-b,b)
-  return gcd(a,b-a)
-
-def gcd2(a,b):
-  if a==0:return b
-  return gcd(b%a,a)
-
-def lcm(a,b):
-  return a*b/gcd(a,b)
-
-print(bezout3(2,3,5))
-print(bezout3(4,6,9))
+print(chain(1,100))
